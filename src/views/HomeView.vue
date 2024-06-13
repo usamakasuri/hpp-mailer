5758b49828a47269a1967690b612d05444818835
Usama Safdar Khan <usamasafdarkhan@gmail.com>
2024-06-13 17:13:04 +0500

    Code Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,12 @@
 import TheWelcome from '../components/TheWelcome.vue'
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header.vue";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 </script>
 
 <template>
   <main>
 <!--    <Header />-->
-    <TheWelcome />
-    <Footer />
-    <SpeedInsights />
+    <TheWelcome></TheWelcome>
+    <Footer></Footer>
   </main>
 </template>
